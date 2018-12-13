d48bf9114a19aea91d3003783e38eaebe6a75463
outpaul <bitanpaul8@gmail.com>
2018-12-14 01:01:13 +0530

    Implemented test set

--- model.py
+++ model.py
@@ -1,22 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 #import tkinter as tk
 from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 boston = load_boston()
+
+# This linear model uses the whole data as a training set
 lr = LinearRegression()
 
 b = pd.DataFrame(boston.data)
 
 lr.fit(b, boston.target)
 
-#Add any other dataset for this model to predict in place of b
-print(lr.predict(b)[:1])
+mse = round(np.mean((lr.predict(b) - boston.target) ** 2),2)
 
-# An ideal plot would be an x = y graph
-plt.scatter(lr.predict(b), boston.target)
-plt.title('Prediction vs Real Housing Price')
-plt.xlabel('Predicted Housing Price')
-plt.ylabel('Real Housing Price')
+# lr Residual Plot
+plt.subplot(2,1,1)
+plt.scatter(lr.predict(b),lr.predict(b) - boston.target)
+plt.title(' lr Residual Plot')
+plt.xlabel('Predicted Price')
+plt.ylabel('Error(mse = ' + str(mse) + ')')
+
+# Implementing training set split on another linear model
+trainX, testX, trainY, testY = train_test_split(b, boston.target, test_size = 0.3333, random_state = 3)
+
+lr2 = LinearRegression()
+
+lr2.fit(trainX, trainY)
+
+mse2 = round(np.mean((lr.predict(testX) - testY) ** 2),2)
+
+# lr2 Residual Plot
+plt.subplot(2,1,2)
+plt.scatter(lr2.predict(trainX), lr2.predict(trainX) - trainY, c = 'b', alpha = 1)
+plt.scatter(lr2.predict(testX), lr2.predict(testX) - testY, c = 'r')
+plt.hlines(y = 0, xmin = 0, xmax = 40)
+plt.xlabel('Predicted Price')
+plt.ylabel('Error(mse = ' + str(mse2) + ')')
+plt.title('lr2 Residual Plot')
+
+# Displaying the plots
 plt.show()
